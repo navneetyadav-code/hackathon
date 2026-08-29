d367699bd34176073ec6a9615a43dd1f0afe153e
Navneet Kumar Yadav <navneetyadav982008@gmail.com>
2026-08-30 04:13:48 +0530

    added dashboard_data.php for realtime listing

--- dashboard_seek.php
+++ dashboard_seek.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-require_once __DIR__ . '/config/app.php';
+require_once __DIR__ . '/config/config.php';
 if (!isset($_SESSION['user_id'])) {
     header('Location: login.php');
     exit;
