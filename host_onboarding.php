07faf24f4b68778c6c139bd10a8d4304745867cd
Navneet Kumar Yadav <navneetyadav982008@gmail.com>
2026-08-30 02:14:56 +0530

    fix onboarding

--- host_onboarding.php
+++ host_onboarding.php
@@ -1,7 +1,6 @@
 <?php
 session_start();
-require_once __DIR__ . '/config/config.php';
-
+require_once config/config.php;
 if (!isset($_SESSION['user_id'])) {
     header('Location: login.php');
     exit;
